e9094cfb36963ed854a7900d6c3a1222b710f1e8
Shivam <sshivamkumar177@gmail.com>
2026-02-15 12:56:00 +0530

    dockerize

--- internship-backend/server.js
+++ internship-backend/server.js
@@ -30,7 +30,7 @@ app.get("/health", (req, res) => {
   res.json({ status: "API OK" });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
